3ba08940015a805d479896902ae92120aeb13023
Fida Hussain <fida.hussain@systemsltd.com>
2021-08-30 20:40:58 +0500

    Added first time

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES:Hero[]=[
+    {id:11,name:"Dr Nice0"},
+    {id:12,name:'John'},
+    {id:13,name:'smith'},
+    {id:14,name:"Celeritas"},
+    {id:15,name:"RubberMan"},
+    {id:16,name:"Dynama"},
+    {id:17,name:"Dr IQ"},
+    {id:18,name:"Magma"},
+    {id:19,name:"Bombasto"},
+    {id:20,name:"Tornado"},
+]
